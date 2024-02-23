6457c4b20a162d42e88d457303290762f46da6c2
bstee615 <benjaminjsteenhoek@gmail.com>
2024-02-23 14:41:03 -0600

    Add noop option

--- tree_cutter/__init__.py
+++ tree_cutter/__init__.py
@@ -4,4 +4,7 @@ from tree_cutter.remove_comments import remove_comments
 from tree_cutter.remove_preprocessor_directives import remove_preprocessor_directives
 from tree_cutter.remove_globals import remove_globals
 
-TRANSFORM_MAP = {f.__name__: f for f in [inline_function, remove_blank_lines, remove_comments, remove_preprocessor_directives, remove_globals]}
+def noop(code):
+    return code
+
+TRANSFORM_MAP = {f.__name__: f for f in [inline_function, remove_blank_lines, remove_comments, remove_preprocessor_directives, remove_globals, noop]}
